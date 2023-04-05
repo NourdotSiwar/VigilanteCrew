c6c5598ec178033ce036706d2343b69cfe2effcf
Nour Siwar <nour.siwar@outlook.com>
2023-04-05 13:10:28 -0700

    fixed orange and yellow issue

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,11 +5,11 @@ import './SideBar.css'
 const SideBar = () => {
       return (
             <div className="sideBarDiv">
-                        <Link style={{color: 'white'}} to="/"><h1>Home</h1></Link>
+                        <Link style={{color: 'white', textDecoration: "none"}} to="/"><h1>Home</h1></Link>
                         <br></br>
-                        <Link style={{color: 'white'}} to="/create"><h1>Create a Vigilante!</h1></Link>
+                        <Link style={{color: 'white', textDecoration: "none"}} to="/create"><h1>Create a Vigilante!</h1></Link>
                         <br></br>
-                        <Link style={{color: 'white'}} to="/gallery"><h1>Vigilante Crew</h1></Link>
+                        <Link style={{color: 'white', textDecoration: "none"}} to="/gallery"><h1>Vigilante Crew</h1></Link>
                   </div>
       )
 }
--- src/pages/CrewGallery.jsx
+++ src/pages/CrewGallery.jsx
@@ -20,26 +20,28 @@ const CrewGallery = () => {
         fetchPosts().catch(console.error);
     }, []);
     
-    console.log(posts);
     return (
         <div className="CrewGallery">
             {
                 posts && posts.length > 0 ?
                 posts.map((post,index) => 
-                <div className="CrewGalleryCard" key={index} style={{ boxShadow: `0 0 5px ${post.color}` }}>
-                    <h1>Name: {post.name}</h1>
-                    <h2>Code Name: {post.codename}</h2>
-                    <h3>Gender: {post.gender} | Age: {post.age}</h3>
-                    <h3>Main Ability: {post.main_ability}</h3>
-                    <h3>Main Weapon: {post.main_weapon}</h3>
-                    
+                <div className="CrewGalleryCard" key={index} style={{ '--card-shadow-color': post.color }}>
+                <h1>Name: <span className='colorSpan'>{post.name}</span></h1>
+                <h3>Code Name: <span className='colorSpan'>{post.codename}</span></h3>
+                <h3>Gender: <span className='colorSpan'>{post.gender} </span> Age: <span className='colorSpan'>{post.age}</span></h3>
+                <h3>Main Ability: <span className='colorSpan'>{post.main_ability}</span></h3>
+                <h3>Main Weapon: <span className='colorSpan'>{post.main_weapon}</span></h3>
+
+                    <Link to={`/edit/${post.id}`}><button className='CrewGalleryCardButtons'>Edit</button></Link>
                 </div>
+
 
                 ) : 
                 (
                   <div>
                       <h1>Your Crew is Empty!</h1>
-                      <h2><Link to="/create">Create a Vigilante Here</Link></h2>
+                      <h2><Link style={{color: 'white', textShadow: '0 0 2px white'}}
+                       to="/create">Create a Vigilante Here</Link></h2>
                   </div>
               )}
             
